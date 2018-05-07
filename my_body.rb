b29d850bb1fc54bf52eb5b10232f9ebe4b91e2cb
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-05-07 11:25:08 -0600

    VIMVIMVIMVIM

--- my_body.rb
+++ my_body.rb
@@ -24,22 +24,11 @@ And it was all that you've given to me
 I had a vision of love
 And it was all that you've given me
 
-I've realized a dream
-And I visualized
-The love that came to be
-Feel so alive
-I'm so thankful that I've received
-The answer that heaven
-Has sent down to me
+In 2004 Dupri connected again with Usher contributing to Confessions co-writing and co-producing three consecutive singles Billboard Hot 100 number one songs "Burn", "Confessions Part II", and "My Boo".[10] Confessions won Best Contemporary R&B Album and Best R&B Performance by a Duo or Group with Vocals. Confessions has been certified diamond by the Recording Industry Association of America (RIAA) and, as of 2012, has sold 10 million copies in the US and over 20 million copies worldwide.[11]
 
-You treated me kind
-Sweet destiny
-And I'll be eternally grateful
-Holding you so close to me
-Prayed through the nights
-So faithfully
-Knowing the one that I needed
-Would find me eventually
+Dupri in 2005 worked with Mariah Carey on her The Emancipation Of Mimi with the smash hit "We Belong Together". It stayed at number one for fourteen non-consecutive weeks, becoming the second longest running number one song in US chart history, behind Carey's 1996 collaboration with Boyz II Men on "One Sweet Day". We Belong Together" won Grammy's for Best Female R&B Vocal Performance and Best R&B Song. The same year he worked on Wanted with Bow Wow, he co-produced and co-wrote "Let Me Hold You". In 2006, record producer Jermaine Dupri signed Dem Franchize Boyz to his label So So Def after transferring it from Arista Records to Virgin Records. The following year the group released On Top of Our Game which topped the US Top Rap Albums with the hit songs "I Think They Like Me" and "Lean wit It, Rock wit it".
+
+In October 2007, he published his memoir, "Young, Rich and Dangerous: The Making of a Music Mogul" (Atria Books). In November 2007 co-prouduced and co-wrote with No I.D. this time on Jay-Z's tenth studio album American Gangster. The two contributed to the songs "Success" and "Fallin".
 
 I had a vision of love
 And it was all that you've given to me
