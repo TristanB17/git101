906fa3f917e93648303eef01f8ec65a78137e4bc
Andrew Mockett <califormula@gmail.com>
2018-05-07 11:20:04 -0600

    All I want for Christmas, is to understand git.

--- my_body.rb
+++ my_body.rb
@@ -1,48 +1,62 @@
-Treated me kind
-Sweet destiny
-Carried me through desperation
-To the one that was waiting for me
-It took so long
-Still I believed
-Somehow the one that I needed
-Would find me eventually
+I don't want a lot for Christmas
+There is just one thing I need
+I don't care about the presents
+Underneath the Christmas tree
 
-I had a vision of love
-And it was all that you've given to me
+I just want you for my own
+More than you could ever know
+Make my wish come true
+All I want for Christmas is you, yeah.
 
-Prayed through the nights
-Felt so alone
-Suffered from alienation
-Carried the weight on my own
-Had to be strong
-So I believed
-And now I know I've succeded
-In finding the place I conceived
+I don't want a lot for Christmas
+There is just one thing I need
+And I don't care about the presents
+Underneath the Christmas tree
 
-I had a vision of love
-And it was all that you've given to me
-I had a vision of love
-And it was all that you've given me
+I don't need to hang my stocking
+There upon the fireplace
+Santa Claus won't make me happy
+With a toy on Christmas Day
 
-I've realized a dream
-And I visualized
-The love that came to be
-Feel so alive
-I'm so thankful that I've received
-The answer that heaven
-Has sent down to me
+I just want you for my own
+More than you could ever know
+Make my wish come true
+All I want for Christmas is you
+You, baby
 
-You treated me kind
-Sweet destiny
-And I'll be eternally grateful
-Holding you so close to me
-Prayed through the nights
-So faithfully
-Knowing the one that I needed
-Would find me eventually
+Oh, I won't ask for much this Christmas
+I won't even wish for snow
+And I'm just gonna keep on waiting
+Underneath the mistletoe
 
-I had a vision of love
-And it was all that you've given to me
-I had a vision of love
-And it was all that you
-Turned out to be
+I won't make a list and send it
+To the North Pole for Saint Nick
+I won't even stay awake to
+Hear those magic reindeer click
+
+'Cause I just want you here tonight
+Holding on to me so tight
+What more can I do?
+Baby, all I want for Christmas is you
+You, baby
+
+Oh, all the lights are shining so brightly everywhere
+And the sound of children's laughter fills the air
+
+And everyone is singing
+I hear those sleigh bells ringing
+Santa, won't you bring me the one I really need?
+Won't you please bring my baby to me?
+
+Oh, I don't want a lot for Christmas
+This is all I'm asking for
+I just wanna see my baby
+Standing right outside my door
+
+Oh, I just want you for my own
+More than you could ever know
+Make my wish come true
+Baby, all I want for Christmas is you
+You, baby
+
+All I want for Christmas is you, baby [repeat with ad-libs until fade]
